60867a0ba000ad895f80e8351e1930cadecb59e4
Denis Pronin <dannftk@yandex.ru>
2024-09-10 17:07:02 +0300

    mstack/socket.cpp: refactoring
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- mstack/mstack/socket.cpp
+++ mstack/mstack/socket.cpp
@@ -23,28 +23,28 @@ void socket_t::async_read_some(std::span<std::byte>                             
                 this->tcb->on_data_receive.push(f);
 }
 
-void socket_t::async_read(std::span<std::byte>                                          rbuf,
+void socket_t::async_read(std::span<std::byte>                                          buf,
                           std::function<void(boost::system::error_code const&, size_t)> cb) {
-        async_read_complete(rbuf, [nbytesall = rbuf.size(), cb = std::move(cb)](
-                                          auto const& ec, size_t nbytes_left) {
+        async_read_complete(buf, [nbytesall = buf.size(), cb = std::move(cb)](auto const& ec,
+                                                                              size_t nbytes_left) {
                 cb(ec, nbytesall - nbytes_left);
         });
 }
 
 void socket_t::async_read_complete(
-        std::span<std::byte>                                          rbuf,
+        std::span<std::byte>                                          buf,
         std::function<void(boost::system::error_code const&, size_t)> cb) {
-        async_read_some(rbuf, [this, rbuf, cb = std::move(cb)](boost::system::error_code const& ec,
-                                                               size_t nbytes) mutable {
+        async_read_some(buf, [this, buf, cb = std::move(cb)](boost::system::error_code const& ec,
+                                                             size_t nbytes) mutable {
                 if (ec) {
-                        cb(ec, rbuf.size() - nbytes);
+                        cb(ec, buf.size() - nbytes);
                         return;
                 }
 
-                if (!(nbytes < rbuf.size())) {
+                if (!(nbytes < buf.size())) {
                         cb(boost::system::error_code{std::error_code{}}, 0);
                 } else {
-                        async_read_complete(rbuf.subspan(nbytes), std::move(cb));
+                        async_read_complete(buf.subspan(nbytes), std::move(cb));
                 }
         });
 }
