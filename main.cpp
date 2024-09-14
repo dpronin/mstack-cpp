db9fbecea980145c877c1cf467ea19442ac6a975
Denis Pronin <dannftk@yandex.ru>
2024-09-14 16:25:11 +0300

    update mstack/api.hpp
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include <utility>
 
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/steady_timer.hpp>
 #include <boost/system/detail/error_code.hpp>
 
 #include <spdlog/common.h>
@@ -67,63 +66,6 @@ void do_accept(boost::asio::io_context& io_ctx, std::shared_ptr<mstack::acceptor
         });
 }
 
-template <typename Device, size_t N, typename Receiver>
-void async_dev_read(std::shared_ptr<Device>                   tundev,
-                    std::unique_ptr<std::array<std::byte, N>> buf,
-                    Receiver                                  receiver) {
-        auto* p_dev{tundev.get()};
-        auto* p_buf{buf.get()};
-        p_dev->async_read_some(*p_buf, [tundev = std::move(tundev), receiver = std::move(receiver),
-                                        buf = std::move(buf)](boost::system::error_code const& ec,
-                                                              size_t nbytes) mutable {
-                if (ec) {
-                        spdlog::error("[RECEIVE FAIL] {}", ec.what());
-                        return;
-                }
-                spdlog::debug("[RECEIVE] {}", nbytes);
-                receiver({buf->data(), nbytes});
-                async_dev_read(std::move(tundev), std::move(buf), std::move(receiver));
-        });
-}
-
-template <typename Device, size_t N, typename Fetcher>
-void async_dev_write_tick(std::shared_ptr<Device>                   tundev,
-                          std::unique_ptr<std::array<std::byte, N>> buf,
-                          Fetcher                                   fetcher) {
-        if (auto pkt{fetcher()}) {
-                auto          raw{mstack::raw_packet{.buffer = std::move(pkt->buffer)}};
-                ssize_t const len{raw.buffer->export_data(*buf)};
-                auto*         p_dev{tundev.get()};
-                auto*         p_buf{buf.get()};
-                p_dev->async_write(
-                        {p_buf->data(), static_cast<size_t>(len)},
-                        [tundev = std::move(tundev), buf = std::move(buf),
-                         fetcher = std::move(fetcher)](boost::system::error_code const& ec,
-                                                       size_t nbytes [[maybe_unused]]) mutable {
-                                if (ec) {
-                                        spdlog::error("[WRITE FAIL] {}", ec.what());
-                                        return;
-                                }
-                                async_dev_write_tick(std::move(tundev), std::move(buf),
-                                                     std::move(fetcher));
-                        });
-        } else {
-                using namespace std::chrono_literals;
-                auto t{std::make_unique<boost::asio::steady_timer>(tundev->get_executor())};
-                t->expires_after(100ms);
-                auto* p_t{t.get()};
-                p_t->async_wait([tundev = std::move(tundev), buf = std::move(buf),
-                                 fetcher = std::move(fetcher),
-                                 t = std::move(t)](boost::system::error_code const& ec) mutable {
-                        if (ec) {
-                                spdlog::error("[WRITE TICK FAIL] {}", ec.what());
-                                return;
-                        }
-                        async_dev_write_tick(std::move(tundev), std::move(buf), std::move(fetcher));
-                });
-        }
-}
-
 }  // namespace
 
 int main(int argc, char* argv[]) {
@@ -140,30 +82,30 @@ int main(int argc, char* argv[]) {
         if (!(argc < 4) && std::string_view{argv[1]} == "TUN") {
                 auto tundev{std::make_shared<mstack::tun>(io_ctx)};
                 tundev->capture(std::string_view{argv[2]});
-                async_dev_read(tundev,
-                               std::make_unique_for_overwrite<std::array<std::byte, 1500>>(),
-                               [](std::span<std::byte> buf) {
-                                       mstack::ipv4::instance().receive(mstack::raw_packet{
-                                               .buffer = std::make_unique<mstack::base_packet>(buf),
-                                       });
-                               });
-                async_dev_write_tick(tundev,
-                                     std::make_unique_for_overwrite<std::array<std::byte, 1500>>(),
-                                     [] { return mstack::ipv4::instance().gather_packet(); });
+                mstack::async_dev_read(
+                        tundev, std::make_unique_for_overwrite<std::array<std::byte, 1500>>(),
+                        [](std::span<std::byte> buf) {
+                                mstack::ipv4::instance().receive(mstack::raw_packet{
+                                        .buffer = std::make_unique<mstack::base_packet>(buf),
+                                });
+                        });
+                mstack::async_dev_write_tick(
+                        tundev, std::make_unique_for_overwrite<std::array<std::byte, 1500>>(),
+                        [] { return mstack::ipv4::instance().gather_packet(); });
         } else {
                 auto tapdev{std::make_shared<mstack::tap>(io_ctx)};
                 tapdev->capture(std::string_view{argv[2]});
                 tapdev->set_ipv4_addr(std::string_view{argv[2]});
-                async_dev_read(tapdev,
-                               std::make_unique_for_overwrite<std::array<std::byte, 1500>>(),
-                               [](std::span<std::byte> buf) {
-                                       mstack::ethernetv2::instance().receive(mstack::raw_packet{
-                                               .buffer = std::make_unique<mstack::base_packet>(buf),
-                                       });
-                               });
-                async_dev_write_tick(tapdev,
-                                     std::make_unique_for_overwrite<std::array<std::byte, 1500>>(),
-                                     [] { return mstack::ethernetv2::instance().gather_packet(); });
+                mstack::async_dev_read(
+                        tapdev, std::make_unique_for_overwrite<std::array<std::byte, 1500>>(),
+                        [](std::span<std::byte> buf) {
+                                mstack::ethernetv2::instance().receive(mstack::raw_packet{
+                                        .buffer = std::make_unique<mstack::base_packet>(buf),
+                                });
+                        });
+                mstack::async_dev_write_tick(
+                        tapdev, std::make_unique_for_overwrite<std::array<std::byte, 1500>>(),
+                        [] { return mstack::ethernetv2::instance().gather_packet(); });
         }
 
         uint16_t port{0};
--- mstack/mstack/api.hpp
+++ mstack/mstack/api.hpp
@@ -2,7 +2,13 @@
 
 #include <cstddef>
 
+#include <chrono>
+#include <utility>
+
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
+
+#include <spdlog/spdlog.h>
 
 #include "arp.hpp"
 #include "ethernet.hpp"
@@ -56,6 +62,63 @@ inline void init_stack() {
         ipv4_stack_create();
         arpv4_stack_create();
         ethernetv2_stack_create();
+}
+
+template <typename Device, size_t N, typename Receiver>
+void async_dev_read(std::shared_ptr<Device>                   tundev,
+                    std::unique_ptr<std::array<std::byte, N>> buf,
+                    Receiver                                  receiver) {
+        auto* p_dev{tundev.get()};
+        auto* p_buf{buf.get()};
+        p_dev->async_read_some(*p_buf, [tundev = std::move(tundev), receiver = std::move(receiver),
+                                        buf = std::move(buf)](boost::system::error_code const& ec,
+                                                              size_t nbytes) mutable {
+                if (ec) {
+                        spdlog::error("[RECEIVE FAIL] {}", ec.what());
+                        return;
+                }
+                spdlog::debug("[RECEIVE] {}", nbytes);
+                receiver({buf->data(), nbytes});
+                async_dev_read(std::move(tundev), std::move(buf), std::move(receiver));
+        });
+}
+
+template <typename Device, size_t N, typename Fetcher>
+void async_dev_write_tick(std::shared_ptr<Device>                   tundev,
+                          std::unique_ptr<std::array<std::byte, N>> buf,
+                          Fetcher                                   fetcher) {
+        if (auto pkt{fetcher()}) {
+                auto          raw{mstack::raw_packet{.buffer = std::move(pkt->buffer)}};
+                ssize_t const len{raw.buffer->export_data(*buf)};
+                auto*         p_dev{tundev.get()};
+                auto*         p_buf{buf.get()};
+                p_dev->async_write(
+                        {p_buf->data(), static_cast<size_t>(len)},
+                        [tundev = std::move(tundev), buf = std::move(buf),
+                         fetcher = std::move(fetcher)](boost::system::error_code const& ec,
+                                                       size_t nbytes [[maybe_unused]]) mutable {
+                                if (ec) {
+                                        spdlog::error("[WRITE FAIL] {}", ec.what());
+                                        return;
+                                }
+                                async_dev_write_tick(std::move(tundev), std::move(buf),
+                                                     std::move(fetcher));
+                        });
+        } else {
+                using namespace std::chrono_literals;
+                auto t{std::make_unique<boost::asio::steady_timer>(tundev->get_executor())};
+                t->expires_after(100ms);
+                auto* p_t{t.get()};
+                p_t->async_wait([tundev = std::move(tundev), buf = std::move(buf),
+                                 fetcher = std::move(fetcher),
+                                 t = std::move(t)](boost::system::error_code const& ec) mutable {
+                        if (ec) {
+                                spdlog::error("[WRITE TICK FAIL] {}", ec.what());
+                                return;
+                        }
+                        async_dev_write_tick(std::move(tundev), std::move(buf), std::move(fetcher));
+                });
+        }
 }
 
 }  // namespace mstack
