2559c08d87767cbc50a12292228ec9306cbae509
Denis Pronin <dannftk@yandex.ru>
2024-09-16 11:18:25 +0300

    arp_cache.hpp: refactoring
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- mstack/mstack/arp_cache.hpp
+++ mstack/mstack/arp_cache.hpp
@@ -2,6 +2,7 @@
 
 #include <optional>
 #include <unordered_map>
+#include <utility>
 
 #include "ipv4_addr.hpp"
 #include "mac_addr.hpp"
@@ -10,47 +11,31 @@
 
 namespace mstack {
 
-struct arp_cache_t {
-        std::unordered_map<int, mac_addr_t>         mac_addr_map;
-        std::unordered_map<int, ipv4_addr_t>        ipv4_addr_map;
-        std::unordered_map<ipv4_addr_t, mac_addr_t> arp_cache;
-
+class arp_cache_t {
+public:
         void reset(ipv4_addr_t const& ipv4_addr) {
-                if (auto it{arp_cache.find(ipv4_addr)}; arp_cache.end() != it) {
+                if (auto it{cache_.find(ipv4_addr)}; cache_.end() != it) {
                         spdlog::debug("[REMOVE ARP CACHE] {}:{}", it->first, it->second);
-                        arp_cache.erase(it);
+                        cache_.erase(it);
                 }
         }
 
         void update(ipv4_addr_t const& ipv4_addr, mac_addr_t const& mac_addr) {
-                spdlog::debug("[ADD ARP CACHE] {} -> {}", ipv4_addr, mac_addr);
-                arp_cache[ipv4_addr] = mac_addr;
+                spdlog::debug("[UPDATE ARP CACHE] {} -> {}", ipv4_addr, mac_addr);
+                cache_[ipv4_addr] = mac_addr;
         }
 
         void update(std::pair<ipv4_addr_t, mac_addr_t> const& kv) { update(kv.first, kv.second); }
 
         std::optional<mac_addr_t> query(ipv4_addr_t const& ipv4_addr) {
-                if (arp_cache.find(ipv4_addr) == arp_cache.end()) {
-                        return std::nullopt;
+                if (auto it{cache_.find(ipv4_addr)}; cache_.end() != it) {
+                        return it->second;
                 }
-                return arp_cache[ipv4_addr];
+                return std::nullopt;
         }
 
-        std::optional<mac_addr_t> query_dev_mac_addr(int tag) {
-                if (mac_addr_map.find(tag) == mac_addr_map.end()) {
-                        spdlog::error("[UNKONWN DEV MAC]");
-                        return std::nullopt;
-                }
-                return mac_addr_map[tag];
-        }
-
-        std::optional<ipv4_addr_t> query_dev_ipv4_addr(int tag) {
-                if (ipv4_addr_map.find(tag) == ipv4_addr_map.end()) {
-                        spdlog::error("[UNKONWN DEV IPV4]");
-                        return std::nullopt;
-                }
-                return ipv4_addr_map[tag];
-        }
+private:
+        std::unordered_map<ipv4_addr_t, mac_addr_t> cache_;
 };
 
 }  // namespace mstack
